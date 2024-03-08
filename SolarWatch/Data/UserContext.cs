9acd69aef2136c66bb97a89c2965918a14de34f1
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-03-08 11:46:47 +0100

    Identity database set up

---
+++ SolarWatch/Data/UserContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SolarWatch.Data;
+
+public class UserContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+{
+    public UserContext (DbContextOptions<UserContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer("Server=localhost,1433;Database=SolarWatch;User Id=sa;Password=Zakuro19920120;Encrypt=false;TrustServerCertificate=true;");
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
