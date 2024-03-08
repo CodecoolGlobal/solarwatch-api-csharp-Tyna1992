5af691f06efad8181e6994ec9682d38c43037537
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-03-08 17:40:46 +0100

    Navigation bar created

--- SolarWatchClient/src/App.jsx
+++ SolarWatchClient/src/App.jsx
@@ -1,30 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Outlet, Link } from "react-router-dom";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className = "navbar">
-      <nav>
+    <div className = "app">
+        
+      <nav className='navbar'> 
         <ul>
           <Link to = "/">
-            <button type='button'>Home</button>
+            <button className='button' type='button'>Home</button>
           </Link>
           <Link to = "/register">
-            <button type='button'>Register</button>
+            <button className='button' type='button'>Register</button>
           </Link>
           <Link to = "/login">
-            <button type='button'>Login</button>
+            <button className='button' type='button'>Login</button>
           </Link>
           <Link to = "/">
-            <button type='button'>Logout</button>
+            <button className='button' type='button'>Logout</button>
           </Link>
           <Link to = "/solarwatch">
-            <button type='button'>Get solar data</button>
+            <button className='button' type='button'>Get solar data</button>
           </Link>
         </ul>
       </nav>
