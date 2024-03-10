a79c15361edb56c274ab26f74c701e5408956b7a
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-03-10 14:17:23 +0100

    Welcoming message for home page

--- SolarWatchClient/src/Pages/Home.jsx
+++ SolarWatchClient/src/Pages/Home.jsx
@@ -6,7 +6,8 @@ function Home(){
 
     return(
         <div>
-            <h1>Home</h1>
+            <h1>Welcome to the SolarWatch!</h1>
+            <h2>Get sunrise and sunset data of your location!</h2>
         </div>
     )
 }
