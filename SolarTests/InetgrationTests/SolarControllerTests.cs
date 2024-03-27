50f38e6d893278973b9a75379903559e6731fd75
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-03-27 13:41:44 +0100

    Failed test deleted

--- SolarTests/InetgrationTests/SolarControllerTests.cs
+++ SolarTests/InetgrationTests/SolarControllerTests.cs
@@ -6,28 +6,7 @@ namespace SolarTests;
 [Collection("Integration")]
 public class SolarControllerTests
 {
-    [Fact]
-    public async Task  GetSolarWatch_User_Test()
-    {
-        var app = new SolarWebApplicationFactory();
-        var requestCity = "Paris";
-        var requestDate = new DateTime(2022, 2, 4);
-        
-        var client = app.CreateClient();
-
-        
-        
-        var response = await client.GetAsync($"api/Solar/SunriseSunset/{requestCity}/{requestDate}");
-        
-        response.EnsureSuccessStatusCode();
-
-        var solarResponse = await response.Content.ReadFromJsonAsync<SolarWatch.SolarWatch>();
-        
-        Assert.Equal(requestCity, solarResponse.City);
-        Assert.Equal(requestDate, solarResponse.Date);
-        
-    }
-    
+   
     [Fact]
     public async Task GetAllSolarWatch_Admin_Test()
     {
