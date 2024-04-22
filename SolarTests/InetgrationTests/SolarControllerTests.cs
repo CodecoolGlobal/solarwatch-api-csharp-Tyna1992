6f4f8cfd1d4681937cf111f3919b91d4ca97db6a
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-04-22 14:35:46 +0200

    New test added

--- SolarTests/InetgrationTests/SolarControllerTests.cs
+++ SolarTests/InetgrationTests/SolarControllerTests.cs
@@ -1,11 +1,31 @@
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
 
 
 namespace SolarTests;
 [Collection("Integration")]
 public class SolarControllerTests
 {
+
+    [Fact]
+    public async Task GetSolarData()
+    {
+        var app = new SolarWebApplicationFactory();
+        var client = app.CreateClient();
+        var city = "Budapest";
+        var date = new DateTime(2025, 01, 01);
+
+        var response = await client.GetAsync($"/api/Solar/SunriseSunset/{city}/{date}");
+
+        response.EnsureSuccessStatusCode();
+
+        var solarWatch = await response.Content.ReadFromJsonAsync<SolarWatch.SolarWatch>();
+
+        Assert.NotNull(solarWatch);
+        Assert.Equal(solarWatch.City, city);
+        Assert.Equal(solarWatch.Date, date);
+    }
    
     [Fact]
     public async Task GetAllSolarWatch_Admin_Test()
